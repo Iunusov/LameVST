3759899b3ed93977fba27c97a0bbc588238b1a7c
Artur Iunusov <iunusov.artur@gmail.com>
2017-01-14 15:18:10 +0300

    Added MinGW (mingw-w64) toolchain support.

--- main.cpp
+++ main.cpp
@@ -55,24 +55,6 @@ VST_EXPORT AEffect *main_plugin(audioMasterCallback audioMaster) {
 void *hInstance;
 
 extern "C" {
-//------------------------------------------------------------------------
-/** Prototype of the export function main */
-//------------------------------------------------------------------------
-VST_EXPORT int main(audioMasterCallback audioMaster) {
-  // Get VST Version
-  if (!audioMaster(0, audioMasterVersion, 0, 0, 0, 0))
-    return 0; // old version
-
-  // Create the AudioEffect
-  AudioEffect *effect = createEffectInstance(audioMaster);
-  if (!effect)
-    return 0;
-
-  return (int)effect->getAeffect();
-}
-} // extern "C"
-
-extern "C" {
 BOOL WINAPI DllMain(HINSTANCE hInst, DWORD dwReason, LPVOID lpvReserved) {
   hInstance = hInst;
   return 1;
